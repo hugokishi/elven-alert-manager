cc43edfd4264abe587fa8c6d25553d40ae8a7039
hugokishi <hugopacheco370@gmail.com>
2022-04-08 17:58:43 -0300

    initial commit

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+export type PrometheusMetricsExporter = {
+  /**
+   * ! Use this function if you want to export the logs without any kind of alert
+   */
+  Alert: (message: string) => void;
+  /**
+   * ! Use this function if you want to export the logs and warn of a possible error
+   */
+  Warning: (message: string) => void;
+};
+
+/**
+ * ! Start a new Gauge in prom-client
+ */
+export function init(): PrometheusMetricsExporter;
---
+++ index.js
@@ -0,0 +1,44 @@
+const { Gauge } = require("prom-client");
+
+let prometheusMetricsExporter;
+
+/**
+ * ! Use this function if you want to export the logs without any kind of alert
+ */
+const Warning = (message) => {
+  return prometheusMetricsExporter.inc({ level: "warning", message }, 1);
+};
+
+/**
+ * ! Use this function if you want to export the logs and warn of a possible error
+ */
+const Alert = (message) => {
+  return prometheusMetricsExporter.inc({ level: "alert", message }, 1);
+};
+
+/**
+ * Start prometheus metrics exporter
+ */
+const init = () => {
+  if (prometheusMetricsExporter) {
+    return {
+      prometheusMetricsExporter,
+      Alert,
+      Warning,
+    };
+  }
+
+  prometheusMetricsExporter = new Gauge({
+    name: "winston_logs_bucket",
+    help: "Returns logger level and message in metrics",
+    labelNames: ["level", "message"],
+  });
+
+  return {
+    prometheusMetricsExporter,
+    Alert,
+    Warning,
+  };
+};
+
+module.exports = { init };
